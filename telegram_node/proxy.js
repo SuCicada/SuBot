53b67b2863dc0b5718e0421e4c45efba2dff961a
pengyifu <yifu.peng@mail.hypers.com>
2021-01-14 17:04:54 +0800

    bugfix 代理设置

--- telegram_node/proxy.js
+++ telegram_node/proxy.js
@@ -1,5 +1,5 @@
 require('global-agent/bootstrap')
-let proxy = require('config').PROXY
+let proxy = require('./config').PROXY
 if (proxy) {
     global.GLOBAL_AGENT.HTTP_PROXY = proxy;
     global.GLOBAL_AGENT.HTTPS_PROXY = proxy;
